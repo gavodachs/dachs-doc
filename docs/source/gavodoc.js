f75b98ce6c2f816ecc887f26738051c1df7bf66b
Markus Demleitner <m@tfiu.de>
2022-01-18 13:13:28 +0100

    Expunging jquery for the doc js

--- docs/source/gavodoc.js
+++ docs/source/gavodoc.js
@@ -1,30 +1,39 @@
 // Misc Javascript hacks for GAVO's documentation suite
-
 // display sphinx-like destinations for divs with an id
 
+"use strict"
 
 function _addAnchor(ev) {
-	var anchor = $("<a class='linkAnchor'>¶</a>");
-	anchor[0].setAttribute("href", "#"+this.parentNode.id);
-	this.appendChild(anchor[0]);
+	// ev has fired on the headline; make a link to the parent div.
+	let anchor = document.createElement('a');
+	anchor.className = "linkAnchor";
+	anchor.innerHTML = "¶";
+	anchor.href = "#"+ev.currentTarget.parentNode.id;
+	ev.currentTarget.append(anchor);
 }
 
 
 function _removeAnchor(ev) {
-	$(this).find(".linkAnchor").remove();
-}
-
-
-function _addAnchorShower(index, el) {
-	if (el.getAttribute("id")) {
-		var dest = el.firstChild.nextSibling;
-		$(dest).bind("mouseenter", _addAnchor);
-		$(dest).bind("mouseleave", _removeAnchor);
+	for (let el of ev.currentTarget.querySelectorAll(".linkAnchor")) {
+		el.remove();
 	}
 }
 
 
-$(document).ready(function () {
-	$("div").map(_addAnchorShower);
-});
+function _addAnchorShower(el) {
+	if (el.id) {
+		// this is a div; we'd like to operate on the headline, which is
+		// the second child (the first is a text node).
+		let dest = el.childNodes[1];
+		dest.addEventListener("mouseenter", _addAnchor);
+		dest.addEventListener("mouseleave", _removeAnchor);
+	}
+}
 
+
+document.addEventListener('DOMContentLoaded',
+	function () {
+		for (let el of document.querySelectorAll("div")) {
+			_addAnchorShower(el);
+	}
+});
